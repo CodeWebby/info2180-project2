5a33332865c77e4550bde2c94673bded5af73fab
CodeWebby <43757810+CodeWebby@users.noreply.github.com>
2018-10-31 09:46:08 -0500

    Update fifteen.js
    try to move the position\

--- fifteen.js
+++ fifteen.js
@@ -1,10 +1,30 @@
 window.onload = function fifteen(){
-	
+	var y = 100;
+	var x = 100;
 
 	var element = document.getElementById("puzzlearea").querySelectorAll("div");
-    for (var i = 0; i < 15; i++){
-	element[i].classList.add("puzzlepiece");
-    }
+       for (var i = 0; i < 15; i++){
+	     element[i].classList.add("puzzlepiece");
+         }
+         
+        function ChangeBgPos () {
+            var selects = document.getElementsByClassName ("puzzlepiece");
 
-    
+            
+            var selectStates = [];
+            for (var i=0; i < selects.length; i++) {
+                var whichSelected = selects[i].selectedIndex;
+                selectStates.push (selects[i].options[whichSelected].text);
+            }
+
+            var div = document.getElementByClassName ("puzzlepiece");
+
+            if ('backgroundPositionX' in div.style) {
+                div.style.backgroundPositionX = selectStates[0];
+                div.style.backgroundPositionY = selectStates[1];
+            } else {
+                div.style.backgroundPosition = (selectStates[0] + " " + selectStates[1]);
+            }
+        }
+     
 }
